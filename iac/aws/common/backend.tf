386c5e29c0f7d3339d2844cf5ef80a0178181177
Paula Bassaganas <paula.bassaganas@boehringer-ingelheim.com>
2026-02-10 20:08:25 +0100

    chore: remove legacy infrastructure directories and state files

--- iac/aws/common/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-classroom-shared"
-    key            = "classroom/shared/terraform.tfstate"
-    region         = "eu-west-3"
-    dynamodb_table = "terraform-locks-classroom-shared"
-    encrypt        = true
-  }
-}
--- iac/aws/workshops/testus_patronus/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-testus-patronus"
-    key            = "classroom/testus-patronus/terraform.tfstate"
-    region         = "eu-west-3"
-    dynamodb_table = "terraform-locks-testus-patronus"
-    encrypt        = true
-  }
-}
